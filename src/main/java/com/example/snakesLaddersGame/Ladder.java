d5d3249d840b55ad769b72a4cad1b995d1f6f8b0
shivamswarnim96 <shivamswarnim96@gmail.com>
2021-08-10 00:37:22 +0530

    Added Model classes for Game
    Added Model classes for Game

---
+++ src/main/java/com/example/snakesLaddersGame/Ladder.java
@@ -0,0 +1,20 @@
+package com.example.snakesLaddersGame;
+
+public class Ladder {
+
+    private int start;
+    private int end;
+
+    public Ladder(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
---
+++ src/main/java/com/example/snakesLaddersGame/Player.java
@@ -0,0 +1,22 @@
+package com.example.snakesLaddersGame;
+
+import java.util.UUID;
+
+public class Player {
+
+    private String name;
+    private String id;
+
+    public Player(String name) {
+        this.name = name;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
