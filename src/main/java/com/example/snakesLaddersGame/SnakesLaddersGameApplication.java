77d59d18a04e44cdfa53bbb9e2d8e21f8818150b
shivamswarnim96 <shivamswarnim96@gmail.com>
2021-08-09 16:06:31 +0530

    First commit

---
+++ src/main/java/com/example/snakesLaddersGame/SnakesLaddersGameApplication.java
@@ -0,0 +1,15 @@
+package com.example.snakesLaddersGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesLaddersGameApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakesLaddersGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snakesLaddersGame/SnakesLaddersGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snakesLaddersGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesLaddersGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
